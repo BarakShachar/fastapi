fd825c0abbe0a855cd831741bc3aab602e1d6151
BarakShachar <barakon111@gmail.com>
2023-01-09 18:35:27 +0200

    add sign-up function

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from pydantic import BaseModel
+import firebase_admin
+from firebase_admin import credentials, firestore
 
+cred = credentials.Certificate(".env")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
+
+class User(BaseModel):
+    mail: str
+    name: str
+    isAdmin: bool
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
-
-
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+@app.post("/signup/", status_code=status.HTTP_201_CREATED)
+async def demo_post(user: User):
+    doc_ref = db.ollection("users").document(user.mail)
+    doc_ref.set({
+        "isAdmin": user.isAdmin,
+        "name": user.name,
+        "adminMail": None
+    })
+    return {"message": "user created successfully"}
