7b182010d151b8534789a19aca649f036e2e1803
BarakShachar <barakon111@gmail.com>
2023-01-09 18:54:47 +0200

    .

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ from fastapi import FastAPI, status
 from pydantic import BaseModel
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+from dotenv import load_dotenv
 
-cred = credentials.Certificate("environment")
+load_dotenv()
+cred_dict = {
+    "type": os.getenv("type"),
+    "project_id": os.getenv("project_id"),
+    "private_key_id": os.getenv("private_key_id"),
+    "private_key": os.getenv("private_key"),
+    "client_email": os.getenv("client_email"),
+    "client_id": os.getenv("client_id"),
+    "auth_uri": os.getenv("auth_uri"),
+    "token_uri": os.getenv("token_uri"),
+    "auth_provider_x509_cert_url": os.getenv("auth_provider_x509_cert_url"),
+    "client_x509_cert_url": os.getenv("client_x509_cert_url")
+}
+
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 app = FastAPI()
